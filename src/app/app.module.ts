d38abea30b68d63d933e7efe63b27f753d9d81e8
Innovati <info@innovati.com.co>
2018-09-19 16:01:30 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { FormsModule } from '@angular/forms'
+import { AgmCoreModule } from '@agm/core';
+import { RestaltarDirective } from './directives/resaltar.directive';
+import { ContarClicksDirective } from './directives/contar-clicks.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaltarDirective,
+    ContarClicksDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyATfNJUK0pyUQJQU2tl_SFX7wwN8LA6BFc'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
